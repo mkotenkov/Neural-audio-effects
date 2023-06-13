24bcab9c1e71974ba9931f59998947548f9c2175
Maksim Koltiugin <mkoltugin@gmail.com>
2023-06-13 07:30:10 +0300

    Init

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.example',
-   packages=["modules"],
+   packages=[],
    install_requires=["torch", "torchaudio"],
 )
