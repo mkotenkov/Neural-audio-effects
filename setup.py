d96218dd182ab81c9b7fcc50c56d2ea6529d613c
Maksim Koltiugin <mkoltugin@gmail.com>
2023-06-13 07:56:54 +0300

    Init

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.example',
-   packages=["modules"],
-   install_requires=["torch", "numpy"],
+   packages=["example_package"],
+   install_requires=[],
 )
