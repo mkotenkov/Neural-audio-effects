cae827dc07c68a67f17416b0c56ba5f4eea11e22
Maksim Koltiugin <mkoltugin@gmail.com>
2023-06-13 11:15:58 +0300

    upd

--- dev/modules/__init__.py
+++ dev/modules/__init__.py
@@ -2,3 +2,4 @@ from .IntelligentMerge import IntelligentMerge
 from .CondWaveLearner import CondWaveLearner
 from .TCNBlock import TCNBlock
 from .TCN import TCN
+from .train import train
--- dev/modules/train.py
+++ dev/modules/train.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from tqdm import trange
 
-from dev.modules.TCN import TCN
+from .TCN import TCN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,30 +17,29 @@ def train(
         dilation_growth=10,
         n_layers=3,
         n_channels=17,
-        info_channels=20,
-        cond_size=1,
+        n_waves=20,
+        cond_size=3,
+        min_freq=0.5,
+        max_freq=100,
         dilations=None,
-        channels=None,
-        stereo=False
+        channels=None
 ):
     slice_len = min(x.shape[-1], slice_len)
-    x = x[None, :, :] if stereo else x[None, 0:1, :]
-    y = y[None, :, :] if stereo else y[None, 0:1, :]
+    x = x[None, :, :]
+    y = y[None, :, :]
 
     # build the model
     model = TCN(
-        wave_learner_params=dict(
-            n_waves=info_channels,
-            n_channels=2,
-            cond_size=cond_size,
-            buffer_size=slice_len,
-            min_freq=1,
-            max_freq=20
-        ),
+        n_waves=n_waves,
+        cond_size=cond_size,
+        buffer_size=slice_len,
+        min_freq=min_freq,
+        max_freq=max_freq,
         channels=[n_channels] * n_layers if channels is None else channels,
         dilations=[dilation_growth ** idx for idx in range(n_layers)] if dilations is None else dilations,
-        in_ch=2 if stereo else 1,
-        out_ch=2 if stereo else 1)
+        in_ch=x.shape[1],
+        out_ch=y.shape[1]
+    )
 
     # print info
     rf = model.calc_receptive_field()
@@ -49,16 +48,21 @@ def train(
     print(f"Receptive field: {rf} samples or {(rf / sample_rate) * 1e3:0.1f} ms")
 
     # train
-    map(lambda item: item.to(device), [x, y, model])
+    model = model.to(device)
+    x = x.to(device)
+    y = y.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr)
     scheduler = CosineAnnealingLR(optimizer, T_max=n_iters, eta_min=1e-10)
     losses = []
     for _ in trange(n_iters):
         optimizer.zero_grad()
-        start_idx = torch.randint(0, x.shape[-1] - slice_len, (1,))[0]
-        x_crop = x[..., start_idx:start_idx + slice_len]
-        y_crop = y[..., start_idx:start_idx + slice_len]
-        y_hat = model(x_crop, torch.ones(1, cond_size, device=device))
+        #         start_idx = torch.randint(0, x.shape[-1] - slice_len, (1,))[0]
+        #         x_crop = x[..., start_idx:start_idx + slice_len]
+        #         y_crop = y[..., start_idx:start_idx + slice_len]
+        x_crop = x
+        y_crop = y
+        cond = torch.ones(1, cond_size, device=device)
+        y_hat = model(x_crop, cond)
         loss = F.mse_loss(y_hat[..., rf:], y_crop[..., rf:])
         loss.backward()
         losses.append(loss.detach().cpu())
