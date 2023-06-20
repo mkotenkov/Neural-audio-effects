4a8583e31ba5f56880706e262bf7e7cde1f36d79
Maksim Koltiugin <mkoltugin@gmail.com>
2023-06-20 13:08:18 +0300

    add index.html

--- dev/modules/IntelligentMerge.py
+++ dev/modules/IntelligentMerge.py
@@ -3,30 +3,24 @@ from torch import nn
 
 
 class IntelligentMerge(nn.Module):
-    def __init__(self, in_channels, out_channels, hidden_size, act_func, cond_size):
+    def __init__(self, in_channels, out_channels, hidden_size, act_func):
         super().__init__()
         self.in_channels = in_channels
-        self.cond_size = cond_size
 
         self.model = nn.Sequential(
-            nn.Linear(in_channels + cond_size, hidden_size), act_func,
+            nn.Linear(in_channels, hidden_size), act_func,
             nn.Linear(hidden_size, out_channels)
         )
 
-    def forward(self, x, cond):
+    def forward(self, x):
         assert x.ndim == 3, f"{x.shape}"  # (batch_size, channels, n_samples)
         assert x.shape[1] == self.in_channels, f"{x.shape}"
-        assert cond.ndim == 2, cond.shape  # (batch_size, cond_size)
-        assert cond.shape[1] == self.cond_size, f"{cond.shape[1]} != {self.cond_size}"
 
         batch_size = x.shape[0]
-        n_samples = x.shape[-1]
 
-        cond = torch.stack([cond] * n_samples).permute(1, 2, 0)
-        cat = torch.cat([x, cond], dim=1)
-        cat = cat.permute(0, 2, 1).flatten(end_dim=1)
+        x = x.permute(0, 2, 1).flatten(end_dim=1)
 
-        out = self.model(cat)
+        out = self.model(x)
 
         batches = out.chunk(batch_size)
         stacked = torch.stack(batches)
--- dev/readme.md
+++ dev/readme.md
@@ -1,1 +1,36 @@
 # IN DEVELOPMENT
+
+```python
+class IntelligentMerge(nn.Module):
+    def __init__(self, in_channels, out_channels, hidden_size, act_func, cond_size):
+        super().__init__()
+        self.in_channels = in_channels
+        self.cond_size = cond_size
+
+        self.model = nn.Sequential(
+            nn.Linear(in_channels + cond_size, hidden_size), act_func,
+            nn.Linear(hidden_size, out_channels)
+        )
+
+    def forward(self, x, cond):
+        assert x.ndim == 3, f"{x.shape}"  # (batch_size, channels, n_samples)
+        assert x.shape[1] == self.in_channels, f"{x.shape}"
+        assert cond.ndim == 2, cond.shape  # (batch_size, cond_size)
+        assert cond.shape[1] == self.cond_size, f"{cond.shape[1]} != {self.cond_size}"
+
+        batch_size = x.shape[0]
+        n_samples = x.shape[-1]
+
+        cond = torch.stack([cond] * n_samples).permute(1, 2, 0)
+        cat = torch.cat([x, cond], dim=1)
+        cat = cat.permute(0, 2, 1).flatten(end_dim=1)
+
+        out = self.model(cat)
+
+        batches = out.chunk(batch_size)
+        stacked = torch.stack(batches)
+        result = stacked.permute(0, 2, 1)
+
+        return result
+
+```
